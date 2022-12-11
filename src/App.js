176a562790f2fb74a032841b6f984f6135390ce0
Nikko Pante <cnpante@stamina4space.upd.edu.ph>
2022-12-11 17:23:07 +0800

    add dropdown menu add legends label

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 import './App.css';
+import Legend from './components/Legend';
 import Optionsfield from './components/Optionsfield';
 import data from './data.geojson';
 import { maxParallelImageRequests } from 'mapbox-gl';
-import { Select } from '@mantine/core';
+import { Select, validateJson, Container } from '@mantine/core';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY25wYW50ZSIsImEiOiJjbGI4MzEydWMwaDRjM3dsajc4aTh1aWdnIn0.FvbLc0We1E7oBA--QN644w';
 
@@ -14,12 +15,22 @@ function App() {
   const [map, setMap] = useState(null);
   const [lng, setLng] = useState(122.5705);
   const [lat, setLat] = useState(10.6952);
-  const [zoom, setZoom] = useState(14.7);
+  const [zoom, setZoom] = useState(14.9);
 
   const options = [
     {
-      name: 'Solar',
-      description: 'Solar radiation per sqm (Kwh/m2·year)',
+      name: 'Solar radiation per sqm',
+      description: 'kWh/m2·year',
+      stops: [
+        [218, '#fafa6e'],
+        [923, '#fce34c'],
+        [1047, '#feca2a'],
+        [1137, '#ffb000'],
+        [1216, '#ff9400'],
+        [1301, '#ff7500'],
+        [1398, '#ff5000'],
+        [1696, '#ff0000'],
+      ],
       property: [
         'interpolate',
         ['linear'],
@@ -37,8 +48,18 @@ function App() {
       ],
     },
     {
-      name: 'Total',
-      description: 'Total solar radiation (Kwh/year)',
+      name: 'Total solar radiation',
+      description: 'kWh/year',
+      stops: [
+        [3328.82, '#b5e877'],
+        [4650.70, '#80d582'],
+        [69393.08, '#4fbf8b'],
+        [95149.81, '#1fa890'],
+        [133605.81, '#00908d'],
+        [201606.24, '#017782'],
+        [394596.59, '#1f5f70'],
+        [23832627, '#2a4858'],
+      ],
       property: [
         'interpolate',
         ['linear'],
@@ -119,7 +140,7 @@ function App() {
   };
 
   const changeState = i => {
-    setActive(options[i]);
+    setActive(options[parseInt(i)]);
     map.setPaintProperty(
       'buildings-layer', 
       'fill-extrusion-color', 
@@ -129,16 +150,34 @@ function App() {
   
   return (
     <div className='map'>
-      <div ref={mapContainer} className="map-container">
-        
-       <div className='elements'>
-        <Optionsfield
-          options={options}
-          property={active.property}
-          changeState={changeState}
+      <div className="sidebar">
+        <h4>Iloilo City Proper Building Roofs Solar Potential</h4>
+        <p>Select a layer using the dropdown menu. Click a building
+          to view information.
+        </p>
+        <Select
+          defaultValue='0'
+          onChange={changeState}
+          data={[
+            {
+              value: '0',
+              label: 'Solar Radiation per Sqm',
+            },
+            {
+              value: '1',
+              label: 'Total Solar Radiation',
+            },
+          ]}
         />
+        <Legend active={active} stops={active.stops} />
+        <br/>
+        <small>Data Sources: &nbsp;
+            <a href="https://lipad.dream.upd.edu.ph/">LiPAD</a>, &nbsp;
+            <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, &nbsp;
+            <a href="https://joint-research-centre.ec.europa.eu/pvgis-photovoltaic-geographical-information-system/pvgis-tools/tmy-generator_en">EU Science Hub</a>
+        </small>
       </div>
-     
+      <div ref={mapContainer} className="map-container">
     </div>
     </div>
   );
--- src/components/Legend.js
+++ src/components/Legend.js
@@ -17,7 +17,6 @@ const Legend = (props) => {
     <>
       <div className="bg-white absolute bottom right mr12 mb24 py12 px12 shadow-darken10 round z1 wmax180">
         <div className="mb6">
-          <h2 className="txt-bold txt-s block">{props.active.name}</h2>
           <p className="txt-s color-gray">{props.active.description}</p>
         </div>
         {props.stops.map(renderLegendKeys)}
